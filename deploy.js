e8be5258a15834d48b83b2d1c9e1db08107a64bd
jetsparrow <scram.jet.sparrow@gmail.com>
2020-08-21 00:58:06 +0300

    build process information grooming

--- deploy.js
+++ deploy.js
@@ -1,42 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
-
-const walkDir = function (dir) {
-    var results = [];
-    var list = fs.readdirSync(dir);
-    list.forEach(function (file) {
-        file = dir + '/' + file;
-        var stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) {
-            /* Recurse into a subdirectory */
-            results = results.concat(walkDir(file));
-        } else {
-            /* Is a file */
-            results.push(file);
-        }
-    });
-    return results;
-}
-
-const createPath = function (filepath) {
-    var dirname = path.dirname(filepath);
-    if (!fs.existsSync(dirname))
-        fs.mkdirSync(dirname, { recursive: true });
-
-}
-
-const copyDir = function (oldPath, newPath) {
-    if (!fs.existsSync(oldPath))
-        return;
-    var tree = walkDir(oldPath);
-    for (const file of tree) {
-        const name = file.substring(oldPath.length);
-        const newName = path.join(newPath, name);
-        createPath(newName);
-        fs.copyFileSync(file, newName);
-    }
-}
+const pathEx = require("./.malanya/pathExtra");
 
 var devPath = JSON.parse(fs.readFileSync("buildinfo.json")).devpath;
 
@@ -55,8 +20,8 @@ for (const dp of datapacks) {
     }
 
     if (fs.existsSync(path.join(dp, "out")))
-        copyDir(path.join(dp, "out", dp), path.join(devPath, dp));
+        pathEx.copyDir(path.join(dp, "out", dp), path.join(devPath, dp));
     else
-        copyDir(dp, path.join(devPath, dp));
-    console.log(`Copied ${dp}`);
+        pathEx.copyDir(dp, path.join(devPath, dp));
+    console.log(`[${dp}] Deployed to dev world`);
 }
