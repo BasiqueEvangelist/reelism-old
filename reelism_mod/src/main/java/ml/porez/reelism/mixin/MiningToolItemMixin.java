b97cdb5f581f7d0856a4ce5288f4083e59fcfbe8
Basique Evangelist <basiqueevangelist@yandex.ru>
2020-10-06 00:24:51 +0300

    Make tools not take damage when not used properly

---
+++ reelism_mod/src/main/java/ml/porez/reelism/mixin/MiningToolItemMixin.java
@@ -0,0 +1,37 @@
+package ml.porez.reelism.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Set;
+import java.util.function.Consumer;
+
+@Mixin(MiningToolItem.class)
+public abstract class MiningToolItemMixin extends Item {
+    @Shadow @Final private Set<Block> effectiveBlocks;
+
+    public MiningToolItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Redirect(method = "postHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V"))
+    public void noDamageOnHit(ItemStack st, int amount, LivingEntity entity, Consumer<LivingEntity> onBreak) {
+
+    }
+
+    @Redirect(method = "postMine", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V"))
+    public void noDamageOnNonEffective(ItemStack st, int amount, LivingEntity entity, Consumer<LivingEntity> onBreak, ItemStack st2, World world, BlockState state) {
+        if (isEffectiveOn(state) || effectiveBlocks.contains(state.getBlock()))
+            st.damage(amount, entity, onBreak);
+    }
+}
---
+++ reelism_mod/src/main/java/ml/porez/reelism/mixin/SwordItemMixin.java
@@ -0,0 +1,18 @@
+package ml.porez.reelism.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.Consumer;
+
+@Mixin(SwordItem.class)
+public class SwordItemMixin {
+    @Redirect(method = "postMine", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V"))
+    public void noDamageOnHit(ItemStack st, int amount, LivingEntity entity, Consumer<LivingEntity> onBreak) {
+
+    }
+}
