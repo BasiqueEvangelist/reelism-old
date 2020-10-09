1bd708a0e13b797f85a08006a93adb5f68eca157
Basique Evangelist <basiqueevangelist@yandex.ru>
2020-10-09 20:04:37 +0300

    Allow disabling shield block on explosion

---
+++ reelism_mod/src/main/java/ml/porez/reelism/mixin/LivingEntityMixin.java
@@ -0,0 +1,17 @@
+package ml.porez.reelism.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+    @Inject(method = "blockedByShield", at = @At(value = "HEAD"), cancellable = true)
+    public void disableExplosionBlock(DamageSource source, CallbackInfoReturnable<Boolean> cb) {
+        if (source.isExplosive())
+            cb.setReturnValue(false);
+    }
+}
