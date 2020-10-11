22b917729b26ec4f48720efbeaefff77c298ddf8
Basique Evangelist <basiqueevangelist@yandex.ru>
2020-10-11 03:48:22 +0300

    Make creepers change grass into mycelium

--- reelism_mod/src/main/java/ml/porez/reelism/mixin/CreeperEntityMixin.java
+++ reelism_mod/src/main/java/ml/porez/reelism/mixin/CreeperEntityMixin.java
@@ -17,10 +17,13 @@ import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin extends HostileEntity {
@@ -48,11 +51,14 @@ public abstract class CreeperEntityMixin extends HostileEntity {
         }
     }
 
+    @Unique
+    private static final List<Block> REPLACABLE_BLOCKS = ImmutableList.of(Blocks.GRASS_BLOCK, Blocks.GRASS_PATH, Blocks.PODZOL);
+
     @Redirect(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"))
     public Explosion diffExplosion(World w, Entity e, double x, double y, double z, float power, Explosion.DestructionType type) {
         if (!Reelism.getConfig().plantCreepers)
             return w.createExplosion(e, x, y, z, power, type);
-        Explosion expl = w.createExplosion(e, x, y, z, power / 2, Explosion.DestructionType.NONE);
+        Explosion expl = w.createExplosion(e, x, y, z, power, Explosion.DestructionType.NONE);
         for (Entity aff : ((ExplosionAccess)expl).reelism$getAffectedEntities()) {
             if (aff instanceof LivingEntity) {
                 LivingEntity liv = (LivingEntity) aff;
@@ -62,6 +68,12 @@ public abstract class CreeperEntityMixin extends HostileEntity {
                 int poisonLength = w.getDifficulty() == Difficulty.HARD ? 15 : (w.getDifficulty() == Difficulty.NORMAL ? 7 : 0);
                 if (poisonLength > 0)
                     liv.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, poisonLength * 20));
+            }
+        }
+        for (BlockPos block : ((ExplosionAccess) expl).reelism$getAffectedBlocks()) {
+            if (REPLACABLE_BLOCKS.contains(w.getBlockState(block).getBlock())) {
+                w.setBlockState(block, Blocks.MYCELIUM.getDefaultState(), 2);
+                w.updateNeighbors(block, Blocks.MYCELIUM);
             }
         }
         return expl;
